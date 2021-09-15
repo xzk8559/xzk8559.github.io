bb0ed18ce609925668d8de123032adafb6f59e36
xzk8559 <xzk8559@163.com>
2021-09-15 14:37:35 +0800

    update 4.2

--- buildinggenerater_merge_3.js
+++ buildinggenerater_merge_3.js
@@ -4,16 +4,32 @@ import { Earcut } from './Earcut.js';
 import { BufferGeometryUtils } from './three.js_130/examples/jsm/utils/BufferGeometryUtils.js';
 
 
-export function BLOCKBuilding_merge(cityMap, dcj, IDR_type) {
+export function BLOCKBuilding_merge(cityMap, dcj, eid, IDR_type) {
+    /**
+     * @param {Object} cityMap - Metadata of city
+     *
+     * @param {Object} dcj - Metadata of responses
+     * @param {Array} dcj.bid - IDs of buildings with response simulated - shape (num_bu)
+     * @param {Array} dcj.dcj_max - An array with shape (num_eq, num_bu)
+     * @param {Array} dcj.dcj_res - An array with shape (num_eq, num_bu)
+     * @param {Array} dcj.dcj_max_top - Upper limit of dcj_max - shape (num_eq)
+     * @param {Array} dcj.dcj_res_top - Upper limit of dcj_res - shape (num_eq)
+     *
+     * @param {number} eid - ID of earthquake simulated
+     * @param {string} IDR_type - choose for max or res
+    **/
 
-    let IDR;
-    if ( IDR_type === 'maximum' ){ IDR = dcj.dcj_max }
-    else{ IDR = dcj.dcj_res }
-    let max_IDR = Math.max( ...IDR );
+    let IDR, max_IDR;
+    if ( IDR_type === 'maximum' ){ IDR = dcj.dcj_max[eid]; max_IDR = dcj.dcj_max_top[eid] }
+    else{ IDR = dcj.dcj_res[eid]; max_IDR = dcj.dcj_res_top[eid] }
+
+    let bid_list = dcj.bid;
+    // let num_bu = dcj.num_bu;
 
     let floor_heights = cityMap.buildings.height;
     let geometries = [];
     for (let ib = 0; ib < cityMap.buildings.number; ib++){
+        let rgb;
         let floor_height = floor_heights[ib];
         let floor = 1;
 
@@ -24,24 +40,17 @@ export function BLOCKBuilding_merge(cityMap, dcj, IDR_type) {
             bounds = cityMap.buildings.bounds[ib],
             pN = bounds.length;
 
-        // let red = getRnd(0.2, 0.8);
-        let red, green, blue
-        if (IDR[ib] < 1e-8){ red = 0.18; green = 0.18; blue = 0.2;}
-        else{
-            red = IDR[ib] / max_IDR * 0.6 + 0.2;
-            green = 1 - red;
-            blue = 0.4;
-        }
+        rgb = get_RGB ( ib, bid_list, IDR, max_IDR );
 
         for (let i0 = 0; i0 < floor + 1; i0++) {
             for (let i1 = 0; i1 < pN; i1++) {
                 let x = bounds[i1][0];
                 let z = bounds[i1][1];
                 let y = i0 * floor_height * cityMap['coordinate scale'];
-                let tmp = (i0 / floor - 1) * 0.03;
+                let tmp = (i0 / floor - 1) * 0.1;
                 vertices.push(x, y, z);
                 // colors.push( 0.18 + tmp, 0.18 + tmp, 0.2 + tmp );
-                colors.push( red + tmp, green + tmp, blue + tmp );
+                colors.push( rgb[0] + tmp, rgb[1] + tmp, rgb[2] + tmp );
                 normals.push( 0, 1, 0.5 );
             }
 
@@ -96,6 +105,19 @@ function exchange0And1( x ){
     return ( 3/2 * x * x - 5/2 * x + 1 )
 }
 
-function getRnd(min, max) {
-    return Math.random() * (max - min) + min;
+// function getRnd(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+function get_RGB ( ib, bid_list, IDR, max_IDR ) {
+    let r, g, b
+    // let r = getRnd(0.2, 0.8);
+    let ind = bid_list.indexOf( ib )
+    if ( ind === -1 ) { r = 0.18; g = 0.18; b = 0.2 }
+    else {
+        r = IDR[ind] / max_IDR * 0.6 + 0.2;
+        g = 1 - r;
+        b = 0.4;
+    }
+    return [r, g, b]
 }
