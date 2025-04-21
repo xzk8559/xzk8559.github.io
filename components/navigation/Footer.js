3e257beff6e3f8f34eb8a7f86b2a7d905cb49f08
Xuzekun <xzk8559@163.com>
2025-04-21 23:33:32 +0800

    Update Footer.js

--- components/navigation/Footer.js
+++ components/navigation/Footer.js
@@ -3,7 +3,7 @@ export default {
   name: 'Footer',
   template: `
     <v-footer app class="grey darken-4" elevation="12">
-      <span>&copy; 2021</span>
+      <span>&copy; 2021 上海同适智能科技有限公司</span>
     </v-footer>
   `
 };
